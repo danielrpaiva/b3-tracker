1f48bf4ebdf3fb4efdf18c9e80477e758c58683b
danielrpaiva <danielpaiva1993@gmail.com>
2024-06-23 22:29:17 -0300

    adiciona arquivos de urls para os apps e adiciona view e template placeholders

--- b3tracker/gui/views.py
+++ b3tracker/gui/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+class MainPage(View):
+    def get(self, request):
+
+        context = {
+            'name': 'WORLD'
+        }
+
+        return render(request, 'mainpage.html', context)
