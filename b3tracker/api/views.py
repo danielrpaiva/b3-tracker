07222f6864b81de50f65475c913f79ac20be5193
danielrpaiva <danielpaiva1993@gmail.com>
2024-06-27 17:16:24 -0300

    Chamadas de listagem dos preços armazenados e de ativos da b3 e disparo de emails

--- b3tracker/api/views.py
+++ b3tracker/api/views.py
@@ -2,8 +2,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 from .tasks import track_b3
-from .models import TrackOrder
-from .serializers import TrackOrderBasicSerializer
+from .models import TrackOrder, OrderQuote
+from .serializers import TrackOrderBasicSerializer, OrderQuoteListSerializer
+from .api_connections.brapi_connector import BrapiApi
 from celery import current_app
 
 import logging
@@ -29,6 +30,9 @@ class TrackerView(APIView):
 
     PUT:
     Interrompe o monitoramento associado a task do id passado
+
+    GET:
+    Busca as cotações armazenadas
     """
     def post(self, request):
         try:
@@ -68,9 +72,69 @@ class TrackerView(APIView):
             logging.exception(str(e))
             return Response({"message": f"Ocorreu um erro inesperado durante a interrupção do monitoramento"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+    def get(self, request):
 
-# TODO: View para listar os preços armazenados dos ativos
-# TODO: View para listar todos os ativos da B3 para o usuario escolher quais ele quer monitorar
+        try:
+            email_param = request.GET.get("email")
+            ticker_param = request.GET.get("ticker")
+            task_id_param = request.GET.get("task")
+            min_price_param = request.GET.get("min_price")
+            max_price_param = request.GET.get("max_price")
+
+            curr_order_quotes = OrderQuote.objects.all()
+
+            if email_param:
+                curr_order_quotes = curr_order_quotes.filter(track_order__requester_email=email_param)
+            
+            if ticker_param:
+                curr_order_quotes = curr_order_quotes.filter(track_order__ticker_code=ticker_param)
+            
+            if task_id_param:
+                curr_order_quotes = curr_order_quotes.filter(track_order__task_id=task_id_param)
+
+            if min_price_param:
+                curr_order_quotes = curr_order_quotes.filter(quote_price__gte=min_price_param)
+
+            if max_price_param:
+                curr_order_quotes = curr_order_quotes.filter(quote_price__lte=max_price_param)
+            
+            resp_data = {
+                "quotes": OrderQuoteListSerializer(instance=curr_order_quotes, many=True).data
+            }
+
+            return Response(resp_data, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            logging.exception(str(e))
+            return Response({"message": f"Ocorreu um erro inesperado durante a listagem de preços"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
+class TickerView(APIView):
+
+    def get(self, request):
+
+        try:
+            ticker_param = request.GET.get("search", None)
+
+            params = dict()
+
+            if ticker_param:
+                params["search"] = ticker_param
+
+            brapi_api = BrapiApi()
+            tickers_resp = brapi_api.list_tickers(params)
+
+            all_tickers = sorted(tickers_resp["indexes"] + tickers_resp["stocks"])
+
+            return Response(all_tickers, status=status.HTTP_200_OK)
+        
+        except Exception as e:
+            logging.exception(str(e))
+            return Response({"message": f"Ocorreu um erro inesperado "}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
+# TODO: Colocar o tratamento de erro generico num middleware?
+          
 
 class TaskDebugView(APIView):
     def get(self, request):
